560442495810ce9e3198c7fad0f2cdbf4a39f230
Inna <melikhovainna@gmail.com>
2023-02-08 14:12:09 +0300

    praxis

---
+++ index.js
@@ -0,0 +1,51 @@
+// задача 1
+
+let salary1 = 40000;
+let salary2 = 22000;
+let salary3 = salary1 + salary2;
+console.log(salary3);
+
+// задача 2
+
+console.log ("Владивосток " + 2000)
+
+// задача 3
+
+let messenger = 128; foto = 254; vk = 137; youtube = 201;
+let relax = messenger + foto + vk + youtube;
+console.log (relax);
+
+// задача 4
+
+console.log(2**5)
+
+// задача 5
+
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!")
+
+// задача 6
+
+const greetings = "Привет";
+const name1 = "Кот";
+console.log (greetings + ", " + name1);
+
+// задача 7
+
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+
+time -= 15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву.')
+
+// задача 8
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + ' градус по Фаренгейту - это ' + celsius + ' градуса по Цельсию.');
